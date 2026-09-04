4e066e63ddcade2ed4178bf0271157ab1176e1e3
Wizard-Lizard36 <max@bartle.co.nz>
2026-09-05 00:22:51 +1200

    Fix double.

--- Powercord.service.worker.js
+++ Powercord.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788523833|22770596';
+const CACHE_VERSION = '1788524530|18496487';
 /** @type {string} */
 const CACHE_PREFIX = 'Powercord-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
