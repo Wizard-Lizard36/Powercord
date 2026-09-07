d659f817cf8d405e24bfa2b66cce006d5aa46c2b
Wizard-Lizard36 <max@bartle.co.nz>
2026-09-07 12:16:05 +1200

    Fix invite friends X not closing it.

--- Powercord.service.worker.js
+++ Powercord.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788738420|308094241';
+const CACHE_VERSION = '1788740052|118718193';
 /** @type {string} */
 const CACHE_PREFIX = 'Powercord-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
