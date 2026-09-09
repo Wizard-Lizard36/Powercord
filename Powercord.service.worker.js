ca9fd670faca4af4f9a308eb25b3c47142f779f5
Wizard-Lizard36 <max@bartle.co.nz>
2026-09-09 12:38:39 +1200

    Made email verification optional.
    You can now turn it off in settings!

--- Powercord.service.worker.js
+++ Powercord.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788821699|64997313055';
+const CACHE_VERSION = '1788914242|6604449295';
 /** @type {string} */
 const CACHE_PREFIX = 'Powercord-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
